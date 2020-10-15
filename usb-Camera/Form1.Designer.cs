d03ba9e518cd51b43249e02782fee7141d0de5d2
BartoszRudnik <rudnik49@gmail.com>
2020-10-16 01:00:37 +0200

    start & stop camera, saving pictures from camera

---
+++ usb-Camera/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace usb_Camera
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize) (this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 514);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 44);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Zapisz zdjecie";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(93, 514);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 44);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Zapisz wideo";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(761, 489);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 44);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Lokalizacja pliku";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(697, 536);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(211, 22);
+            this.textBox1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 38);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Wybierz urzadzenie";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(93, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(252, 24);
+            this.comboBox1.TabIndex = 5;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 50);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(896, 428);
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(324, 514);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 44);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Start";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(405, 514);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 44);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "Stop";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(570, 514);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(121, 44);
+            this.button5.TabIndex = 9;
+            this.button5.Text = "Wybierz lokalizacje pliku";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(920, 570);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize) (this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.Button button5;
+
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label2;
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+
+        #endregion
+    }
+}
---
+++ usb-Camera/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AForge.Video;
+using AForge.Video.DirectShow;
+
+namespace usb_Camera
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private FilterInfoCollection filterDevices;
+        private VideoCaptureDevice newVideo;
+        private FolderBrowserDialog folderBrowser;
+        private string savePath;
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            filterDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+
+            foreach (FilterInfo device in filterDevices)
+            {
+                comboBox1.Items.Add(device.Name);
+            }
+
+            if(comboBox1.Items.Count > 0)
+                comboBox1.SelectedIndex = 0;
+
+        }
+        
+        private void button3_Click(object sender, EventArgs e)
+        { 
+            newVideo = new VideoCaptureDevice(filterDevices[comboBox1.SelectedIndex].MonikerString);
+
+            newVideo.NewFrame += VideoCaptureDevice_NewFrame;
+            newVideo.Start();
+        }
+
+        private void VideoCaptureDevice_NewFrame(object sender, NewFrameEventArgs e)
+        {
+            pictureBox1.Image = (Bitmap) e.Frame.Clone();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (newVideo.IsRunning)
+            {
+                newVideo.SignalToStop();
+                newVideo.WaitForStop();
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (newVideo.IsRunning)
+            {
+                newVideo.SignalToStop();
+                newVideo.WaitForStop();
+                pictureBox1.Image = null;
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            folderBrowser = new FolderBrowserDialog();
+            folderBrowser.ShowDialog();
+            textBox1.Text = folderBrowser.SelectedPath;
+            savePath = folderBrowser.SelectedPath;
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            newVideo.SignalToStop();
+            newVideo.WaitForStop();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (savePath != null)
+            {
+                pictureBox1.Image.Save(savePath + "\\[UP].jpg");
+                MessageBox.Show("Poprawnie zapisano zdjecie");
+            }
+            else
+            {
+                MessageBox.Show("Nie podano sciezki do zapisu pliku");
+            }
+        }
+        
+    }
+    
+}
