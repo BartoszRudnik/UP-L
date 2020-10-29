36546761b296b225988cf8e8e64a267107198955
BartoszRudnik <rudnik49@gmail.com>
2020-10-29 14:33:53 +0100

    obsluga lewego i prawego przycisku myszki za pomoca joysticka

--- JoyStick/Form1.cs
+++ JoyStick/Form1.cs
@@ -19,11 +19,15 @@ namespace JoyStick
         private SlimDX.DirectInput.Joystick newStick;
         private List <Joystick> sticks = new List<Joystick>();
         private bool[] joystickButtons;
+        private bool leftClick = false;
+        private bool rightClick = false;
         
         [DllImport("user32.dll", CharSet = CharSet.Auto)]
         public static extern void mouse_event(uint flag, uint _x, uint _y, uint btn, uint exinfo);
         private const int MOUSEEVENT_LEFTDOWN = 0X02;
         private const int MOUSEEVENT_LEFTUP = 0X04;
+        private const int MOUSEEVENT_RIGHTDOWN = 0X08;
+        private const int MOUSEEVENT_RIGHTUP = 0X10;
 
         public Form1()
         {
@@ -69,15 +73,57 @@ namespace JoyStick
             newStick = sticks[comboBox1.SelectedIndex];
 
             JoystickState state = newStick.GetCurrentState();
+            joystickButtons = state.GetButtons();
             
             Cursor.Position = new Point(Cursor.Position.X + state.X/3, Cursor.Position.Y + state.Y/3);
-            
+
+            if (joystickButtons[0] == true)
+            {
+                
+                mouse_event(MOUSEEVENT_LEFTDOWN,0,0,0,0);
+                leftClick = true;
+
+            }
+
+            if (leftClick == true)
+            {
+                
+                mouse_event(MOUSEEVENT_LEFTUP,0,0,0,0);
+                leftClick = false;
+                
+            }
+
+            if (joystickButtons[1] == true)
+            {
+                
+                mouse_event(MOUSEEVENT_RIGHTDOWN, 0, 0, 0, 0);
+                rightClick = true;
+
+            }
+
+            if (rightClick == true)
+            {
+                
+                mouse_event(MOUSEEVENT_RIGHTUP, 0, 0 ,0 ,0);
+                rightClick = false;
+
+            }
+
+            if (joystickButtons[2] == true)
+            {
+
+                timer1.Enabled = false;
+
+            }
 
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
 
+            MessageBox.Show(
+                "Przycisk '1' -> lewy przycisk myszki, \nPrzycisk '2' -> prawy przycisk myszki \nPrzycisk '3' -> zakoncz symulowanie");
+                
             timer1.Enabled = true;
 
         }
