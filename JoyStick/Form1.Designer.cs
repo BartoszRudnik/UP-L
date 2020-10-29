a4c2231234e6fc142c755e164b0d9aac9ed5dca0
BartoszRudnik <rudnik49@gmail.com>
2020-10-29 14:06:18 +0100

    wykrywanie urzadzen, poruszanie kursorem przy pomocy galek joysticka

---
+++ JoyStick/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace JoyStick
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Timers.Timer();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize) (this.timer1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(154, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(232, 24);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 548);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(185, 37);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Symuluj dzialanie myszki\r\n";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.SynchronizingObject = this;
+            this.timer1.Elapsed += new System.Timers.ElapsedEventHandler(this.timer1_Elapsed);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(136, 23);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Wykryte urzadzenia\r\n";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(922, 597);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize) (this.timer1)).EndInit();
+            this.ResumeLayout(false);
+        }
+
+        private System.Windows.Forms.Label label1;
+
+        private System.Timers.Timer timer1;
+
+        private System.Windows.Forms.Button button1;
+
+        private System.Windows.Forms.ComboBox comboBox1;
+
+        #endregion
+    }
+}
---
+++ JoyStick/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows.Forms;
+using SlimDX.DirectInput;
+
+namespace JoyStick
+{
+    public partial class Form1 : Form
+    {
+        private DirectInput directInput = new DirectInput();
+        private SlimDX.DirectInput.Joystick newStick;
+        private List <Joystick> sticks = new List<Joystick>();
+        private bool[] joystickButtons;
+        
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        public static extern void mouse_event(uint flag, uint _x, uint _y, uint btn, uint exinfo);
+        private const int MOUSEEVENT_LEFTDOWN = 0X02;
+        private const int MOUSEEVENT_LEFTUP = 0X04;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            foreach (DeviceInstance newDevice in directInput.GetDevices(DeviceClass.GameController, DeviceEnumerationFlags.AttachedOnly))
+            {
+                
+                newStick = new SlimDX.DirectInput.Joystick(directInput, newDevice.InstanceGuid);
+                newStick.Acquire();
+                
+                foreach (DeviceObjectInstance DeviceObject in newStick.GetObjects())
+                {
+                    if((DeviceObject.ObjectType & ObjectDeviceType.Axis) != 0)
+                    {
+                        newStick.GetObjectPropertiesById((int)DeviceObject.ObjectType).SetRange(-100, 100);
+
+                    }
+                }
+                
+                sticks.Add(newStick);
+                comboBox1.Items.Add(newDevice.ProductName);
+                
+            }
+
+            if (comboBox1.Items.Count > 0)
+            {
+                comboBox1.SelectedIndex = 0;
+            }
+            else
+            {
+                MessageBox.Show("Nie wykryto zadnego urzadzenia");
+            }
+            
+        }
+
+        private void simulateMouse()
+        {
+            
+            newStick = sticks[comboBox1.SelectedIndex];
+
+            JoystickState state = newStick.GetCurrentState();
+            
+            Cursor.Position = new Point(Cursor.Position.X + state.X/3, Cursor.Position.Y + state.Y/3);
+            
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            timer1.Enabled = true;
+
+        }
+        private void timer1_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            simulateMouse();
+        }
+    }
+    
+}
