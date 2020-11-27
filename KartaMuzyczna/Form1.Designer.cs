41b985967a7ca98f03f1b11d4de82eba2e8ae14f
BartoszRudnik <rudnik49@gmail.com>
2020-11-27 13:53:34 +0100

    odtwarzanie plików .WAV przy pomocy System.Media.SoundPlayer i WindowsMediaPlayer oraz odtwarzanie plików .MP3 przy pomocy WindowsMediaPlayer

--- KartaMuzyczna/Form1.Designer.cs
+++ KartaMuzyczna/Form1.Designer.cs
@@ -32,6 +32,15 @@
             this.button1 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -60,11 +69,104 @@
             this.label1.TabIndex = 2;
             this.label1.Text = "Naglowek wczytanego pliku";
             // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(327, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(220, 23);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "System.Media.SoundPlayer";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(325, 38);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 34);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Start Player\r\n";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.SystemMediaSoundPlayer_Start);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(446, 38);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(101, 34);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Stop Player";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.SystemMediaSoundPlayer_Stop);
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(327, 94);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(220, 23);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Windows Media Player (*.wav)";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(325, 120);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(100, 31);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Start Player";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.WMP_WAV_Start);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(446, 120);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(101, 31);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "Stop Player";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.WMP_WAV_STOP);
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(327, 167);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(220, 23);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Windows Media Player (*.mp3)";
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(325, 193);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(100, 28);
+            this.button6.TabIndex = 10;
+            this.button6.Text = "Start Player";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.WMP_MP3_Start);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(446, 193);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(101, 28);
+            this.button7.TabIndex = 11;
+            this.button7.Text = "Stop Player";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.WMP_MP3_Stop);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(931, 511);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
@@ -73,6 +175,21 @@
             this.ResumeLayout(false);
             this.PerformLayout();
         }
+
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+
+        private System.Windows.Forms.Label label4;
+
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+
+        private System.Windows.Forms.Label label3;
+
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+
+        private System.Windows.Forms.Label label2;
 
         private System.Windows.Forms.Label label1;
 
--- KartaMuzyczna/Form1.cs
+++ KartaMuzyczna/Form1.cs
@@ -5,14 +5,21 @@ using System.Data;
 using System.Drawing;
 using System.IO;
 using System.Linq;
+using System.Media;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WMPLib;
 
 namespace KartaMuzyczna
 {
     public partial class Form1 : Form
     {
+        
+        private String loadFilePath;
+        private SoundPlayer soundPlayer;
+        private WindowsMediaPlayer windowsPlayer;
+        
         public Form1()
         {
             InitializeComponent();
@@ -20,8 +27,7 @@ namespace KartaMuzyczna
 
         private void WczytajButtonClick(object sender, EventArgs e)
         {
-
-            String loadFilePath;
+            
             textBox1.Clear();
             
             OpenFileDialog loadFile = new OpenFileDialog();
@@ -76,6 +82,74 @@ namespace KartaMuzyczna
                 
             }
 
+        }
+
+        private void SystemMediaSoundPlayer_Start(object sender, EventArgs e)
+        {
+            
+            try
+            {
+                soundPlayer = new SoundPlayer(loadFilePath);
+                soundPlayer.Play();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Wybrano bledny plik");
+            }
+            
+        }
+
+        private void SystemMediaSoundPlayer_Stop(object sender, EventArgs e)
+        {
+            soundPlayer.Stop();
+        }
+
+        private void WMP_MP3_Start(object sender, EventArgs e)
+        {
+            
+            OpenFileDialog newFile = new OpenFileDialog();
+            windowsPlayer = new WindowsMediaPlayer();
+            
+            if (newFile.ShowDialog() == DialogResult.OK)
+            {
+                    String mp3FilePath = newFile.FileName;
+
+                    windowsPlayer.URL = mp3FilePath;
+                    windowsPlayer.controls.play();
+
+            }
+            
+        }
+
+        private void WMP_MP3_Stop(object sender, EventArgs e)
+        {
+            windowsPlayer.controls.stop();
+        }
+
+        private void WMP_WAV_Start(object sender, EventArgs e)
+        {
+
+            try
+            {
+                
+                if(loadFilePath == null)
+                    throw new Exception();
+                
+                windowsPlayer = new WindowsMediaPlayer();
+                windowsPlayer.URL = loadFilePath;
+                windowsPlayer.controls.play();
+                
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Wybrano bledny plik");
+            }
+
+        }
+
+        private void WMP_WAV_STOP(object sender, EventArgs e)
+        {
+            windowsPlayer.controls.stop();
         }
         
     }
