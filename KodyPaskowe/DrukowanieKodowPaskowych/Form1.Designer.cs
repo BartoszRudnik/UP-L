3fb5ce8ef6a7900927d8f35c51fca9f3063c93cc
Bartosz Rudnik <rudnik49@gmail.com>
2021-03-04 09:47:13 +0100

    moved

---
+++ KodyPaskowe/DrukowanieKodowPaskowych/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace DrukowanieKodowPaskowych
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize) (this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(179, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(119, 46);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Podaj kod do wygenerowania";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(95, 49);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(263, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles) ((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) | System.Windows.Forms.AnchorStyles.Left) | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Location = new System.Drawing.Point(75, 86);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(382, 115);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 206);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 49);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Generuj kod paskowy";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.generateBarCode_ButtonClick);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(176, 207);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(122, 49);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Drukuj kod paskowy";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.printBarCode_ButtonClick);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(338, 206);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(119, 49);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Zapisz kod paskowy";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.saveBarCode_ButtonClick);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(120, 289);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(337, 24);
+            this.comboBox1.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(12, 279);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 43);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Dostepne drukarki";
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(12, 337);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(100, 57);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Rozmiar drukowanego kodu";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(120, 361);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(337, 24);
+            this.comboBox2.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(488, 403);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize) (this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.ComboBox comboBox2;
+
+        private System.Windows.Forms.Label label3;
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label2;
+
+        private System.Windows.Forms.Button button3;
+
+        private System.Windows.Forms.Button button2;
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+
+        private System.Windows.Forms.TextBox textBox1;
+
+        private System.Windows.Forms.Label label1;
+
+        #endregion
+    }
+}
---
+++ KodyPaskowe/DrukowanieKodowPaskowych/Form1.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Drawing.Printing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Spire.Barcode;
+
+namespace DrukowanieKodowPaskowych
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            comboBox2.Items.Add("Maly");
+            comboBox2.Items.Add("Sredni");
+            comboBox2.Items.Add("Duzy");
+
+            comboBox2.SelectedIndex = 0;
+            
+            foreach (object printer in PrinterSettings.InstalledPrinters)
+            {
+
+                comboBox1.Items.Add(printer.ToString());
+
+            }
+
+            if (comboBox1.Items.Count > 0)
+            {
+
+                comboBox1.SelectedIndex = 0;
+
+            }
+            
+        }
+
+        private bool codeLength(string kodPaskowy)
+        {
+            if (kodPaskowy.Length != 12)
+                return false;
+            else
+            {
+                return true;
+            }
+        }
+        
+        private static Image usunNapis(Image barCodeImage)
+        {
+            
+            Rectangle newArea = new Rectangle();
+
+            newArea.X = 0;
+            newArea.Y = 13;
+            newArea.Width = barCodeImage.Width;
+            newArea.Height = barCodeImage.Height - 13;
+            
+            Bitmap bmpImage = new Bitmap(barCodeImage);
+            return bmpImage.Clone(newArea, bmpImage.PixelFormat);
+            
+        }
+
+        private void generateBarCode_ButtonClick(object sender, EventArgs e)
+        {
+            string barCode = textBox1.Text.Trim();
+            
+            try
+            {
+                
+                if(!codeLength(barCode))
+                    throw new Exception();
+                
+                BarcodeSettings barcodeSettings = new BarcodeSettings();
+
+                barcodeSettings.Type = BarCodeType.EAN13;
+                barcodeSettings.Data = barCode;
+                barcodeSettings.UseChecksum = CheckSumMode.ForceEnable;
+                barcodeSettings.ShowTextOnBottom = true;
+                barcodeSettings.TextAlignment = StringAlignment.Center;
+                
+                BarCodeGenerator generator = new BarCodeGenerator(barcodeSettings);
+
+                Image barCodeImage = generator.GenerateImage();
+                barCodeImage = usunNapis(barCodeImage);
+
+                pictureBox1.Image = barCodeImage;
+
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Podano kod o zlej dlugosci");
+            }
+
+        }
+
+        private void saveBarCode_ButtonClick(object sender, EventArgs e)
+        {
+
+            try
+            {
+
+                if (pictureBox1.Image == null)
+                    throw new Exception();
+
+                string savePath = "";
+                SaveFileDialog saveFile = new SaveFileDialog();
+                saveFile.DefaultExt = "jpg";
+                saveFile.Filter = "JPG files (*.jpg)|*.jpg|All files (*.*)|*.*";
+
+                if (saveFile.ShowDialog() == DialogResult.OK)
+                {
+                    savePath = saveFile.FileName;
+                }
+
+                MessageBox.Show(savePath);
+
+                pictureBox1.Image.Save(savePath, ImageFormat.Jpeg);
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Nie wygenerowano kodu paskowego");
+            }
+
+        }
+
+        private void printBarCode_ButtonClick(object sender, EventArgs e)
+        {
+            
+            string printer = comboBox1.SelectedItem.ToString();
+            myPrinters.SetDefaultPrinter(printer);
+            
+            PrintDocument printBarCode = new PrintDocument();
+
+            printBarCode.PrintPage += PrintPage;
+            printBarCode.Print();
+
+        }
+
+        private void PrintPage(object o, PrintPageEventArgs e)
+        {
+
+            try
+            {
+
+                int imageHeight = 0;
+                int imageWidth = 0;
+
+                if (comboBox2.SelectedItem.Equals("Maly"))
+                {
+                    imageHeight = 100;
+                    imageWidth = 300;
+                }
+                else if (comboBox2.SelectedItem.Equals("Sredni"))
+                {
+                    imageHeight = 200;
+                    imageWidth = 400;
+                }
+                else if (comboBox2.SelectedItem.Equals("Duzy"))
+                {
+                    imageHeight = 300;
+                    imageWidth = 500;
+                }
+
+                Image barCodeImage = pictureBox1.Image;
+
+                Rectangle imageLocation = e.MarginBounds;
+
+                imageLocation.Height = imageHeight;
+                imageLocation.Width = imageWidth;
+
+                e.Graphics.DrawImage(barCodeImage, imageLocation);
+
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message);
+            }
+
+        }
+        
+    }
+    
+    public class myPrinters
+    {
+        [DllImport("winspool.drv", CharSet = CharSet.Auto, SetLastError = true)]
+        public static extern bool SetDefaultPrinter(string Name);
+
+    }
+    
+}
